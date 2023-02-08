056973d90c2441f0af454158464a9c10a5f3304e
BrianMwas <brnmwas@gmail.com>
2023-02-08 10:28:40 +0300

    Created dummy screen

--- feature/welcome/src/main/java/com/quicksnap/welcome/welcome/authentication/signup/SignUpScreen.kt
+++ feature/welcome/src/main/java/com/quicksnap/welcome/welcome/authentication/signup/SignUpScreen.kt
@@ -1,16 +1,23 @@
 package com.quicksnap.welcome.welcome.authentication.signup
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun SignUpScreen(toLogin: () -> Unit) {
-    Box {
-        Text(text = "Sign up Screen")
-        Button(onClick = toLogin) {
-            Text(text = "Login")
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(text = "Sign up Screen")
+            Button(onClick = toLogin) {
+                Text(text = "Login")
+            }
         }
     }
 }
