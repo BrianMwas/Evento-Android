ff135b5b621425148d52534e22db34cecb381ce2
BrianMwas <brnmwas@gmail.com>
2023-03-02 15:34:42 +0300

    Added user repository

--- data/repository/src/main/java/com/quicksnap/repository/authentication/UserRepository.kt
+++ data/repository/src/main/java/com/quicksnap/repository/authentication/UserRepository.kt
@@ -1,9 +1,15 @@
 package com.quicksnap.repository.authentication
 
+import kotlinx.coroutines.flow.Flow
+
 interface UserRepository {
-    fun login()
-    fun signUp()
-    fun forgotPassword(emil: String)
-    fun changePassword(currentPassword: String, newPassword: String)
-    fun updateUser()
+    suspend fun login(email: String, password: String)
+    suspend fun signUp(email: String, password: String, username: String)
+    suspend fun facebookLogin()
+    suspend fun twitterLogin()
+    suspend fun googleLogin()
+    suspend fun forgotPassword(emil: String)
+    suspend fun changePassword(currentPassword: String, newPassword: String)
+    suspend fun updateUser()
+    suspend fun signOut()
 }
