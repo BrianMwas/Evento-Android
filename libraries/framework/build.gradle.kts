0d6e7b98c33e83284a71b0fd1bf3ee484e9a3586
BrianMwas <brnmwas@gmail.com>
2023-03-14 11:32:10 +0300

    Add dependencies for moshi

--- libraries/framework/build.gradle.kts
+++ libraries/framework/build.gradle.kts
@@ -10,5 +10,8 @@ dependencies {
     implementation(SupportLibs.DataStore)
     implementation(platform(Firebase.Bom))
     implementation(Firebase.firestore)
+
+    implementation("com.squareup.moshi:moshi-kotlin:1.13.0")
+    implementation("com.serjltt.moshi:moshi-lazy-adapters:2.2")
     implementation("androidx.core:core-ktx:+")
 }
